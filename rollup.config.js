dddbcffd216f9a257ad450aa4603ab28a2891f51
liaozhongxun <869664233@qq.com>
2021-01-12 17:29:36 +0800

    'updata'

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,8 @@
 import ts from "rollup-plugin-typescript2"; //解析ts插件
 import { nodeResolve } from "@rollup/plugin-node-resolve"; //解析第三方插件
 import replace from "@rollup/plugin-replace"; //替换插件
-import serveb from "rollup-plugin-serve"; //启动本地服务插件
+import serve from "rollup-plugin-serve"; //启动本地服务插件
 import path from "path";
-import serve from "rollup-plugin-serve";
 
 export default {
     input: "src/index.ts", //入口
@@ -27,6 +26,7 @@ export default {
         //替换环境变量
         replace({
             "process.env.NOOD_ENV": JSON.stringify("development"),
+            "process.env.xxx":JSON.stringify("随意替换值")
         }),
         //启动服务
         serve({
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 let a: any = 10;
+let b: any = 100;
 // debugger;
 console.log(a);
+console.log(process.env.NOOD_ENV);
+console.log(process.env.xxx);
